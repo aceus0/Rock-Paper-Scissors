916bcc76040293078b644856e6d2675fbfe86d66
justin <64299580+aceus0@users.noreply.github.com>
2024-04-11 12:53:03 -0400

    cleaned up the code

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ function getComputerChoice(min, max) {
     let number = Math.random() * (max - min) + min;
     number = Math.floor(number);
     return number
-
-    
-
 }
 
 function playerSelection() {
@@ -20,29 +17,14 @@ function playerSelection() {
     }   else {
         console.log("You must choose rock, paper or scissors!")
     }
-
-
-
-    
-
 }
-// 1 is rock, 2 is paper, 3 is scissors
 
-//% 0 means tie, 2 means player win, -2 computer win, -1 player win, 1
+function playGame() {
+    
+    playerChoice = playerSelection()
+    computerChoice = getComputerChoice(1,4)
 
-let wins = 0
-let loss = 0
-let tied = 0
-
-
-playerChoice = playerSelection()
-computerChoice = getComputerChoice(1,4)
-
-console.log(playerChoice)
-console.log(computerChoice)
-
-function playGame(player, computer) {
-    result = ((player - computer) + 3) % 3
+    result = ((playerChoice - computerChoice) + 3) % 3
     console.log(result)
     if (result === 0) {
         console.log("You tied!")
@@ -56,30 +38,21 @@ function playGame(player, computer) {
     }
 }
 
+
+
+// 1 is rock, 2 is paper, 3 is scissors
+
+//% 0 means tie, 2 means player win, -2 computer win, -1 player win, 1
+
+let wins = 0
+let loss = 0
+let tied = 0
+
+
 playGame(playerChoice, computerChoice)
 playGame(playerChoice, computerChoice)
 playGame(playerChoice, computerChoice)
 playGame(playerChoice, computerChoice)
 playGame(playerChoice, computerChoice)
 
-console.log("You won " + wins + "times, lost " + loss + "times and tied " + tied + "times. Good job!")
-/*
-getComputerChoice
-    set 1, 2, 3 to rock, paper, scissor
-    use random function to determine beween 1-3.
-    check for match 
-    return choice
-
-
-playerSelection
-    accept input, 1 for rock, 2 for paper, 3 for scissor.
-    check for match
-    return choice
-
-playGame
-    take playerSelection and computerSelection and compare use if statement to determine winner 
-        if (rock && paper),
-            console.log(paper wins!)
-        else if 
-
-*/
+console.log("You won " + wins + " times, lost " + loss + " times and tied " + tied + " times. Good job!")
