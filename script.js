1823e53aeac8fb962d33e01123826b6556b60957
justin <64299580+aceus0@users.noreply.github.com>
2024-04-16 20:16:37 -0400

    conversion of terminal program into web app complete

--- script.js
+++ script.js
@@ -3,40 +3,79 @@
 function getComputerChoice(min, max) {
     let number = Math.random() * (max - min) + min;
     number = Math.floor(number);
+    console.log(number)
     return number
 }
 
-function playerSelection() {
-    let playerChoice = prompt("Hello player! Please choose rock, paper or scissors!");
-    playerChoice = playerChoice.toLowerCase()
-
-    if (playerChoice == 'rock') {
+function playerSelection(playerChoice) {
+    if (playerChoice == 'Rock') {
         return choice = 1;
-    }   else if (playerChoice == 'paper') {
+    }   else if (playerChoice == 'Paper') {
         return choice = 2;
-    }   else if (playerChoice == 'scissors'){
+    }   else if (playerChoice == 'Scissors'){
         return choice = 3;
     }   else {
         console.log("You must choose rock, paper or scissors!")
     }
 }
 
-function playGame() {
+function playGame(playerChoice) {
     
-    playerChoice = playerSelection()
+    
+
+    playerChoice = playerSelection(playerChoice)
     computerChoice = getComputerChoice(1,4)
+    
+    switch (playerChoice) {
+        case 1 : playerChoiceDisplay.textContent = "🪨";
+        break;
+        case 2 : playerChoiceDisplay.textContent = "📰";
+        break;
+        case 3 : playerChoiceDisplay.textContent = "✂️";
+        break;
+    }
+    switch (computerChoice) {
+        case 1 : computerChoiceDisplay.textContent = "🪨";
+        break;
+        case 2 : computerChoiceDisplay.textContent = "📰";
+        break;
+        case 3 : computerChoiceDisplay.textContent = "✂️";
+        break;
+    }
+
 
     result = ((playerChoice - computerChoice) + 3) % 3
     console.log(result)
     if (result === 0) {
-        window.alert("You tied!");
-        tied++;
+        announcement.textContent = "You tied!";
+        ties.textContent = `Ties: ${++tied}`;
     } else if (result === 1) {
-        window.alert("You won!");
-        wins++;
+        announcement.textContent = "You won!";
+        playerScore.textContent = `Wins: ${++wins}`;
     } else {
-        window.alert("You lost!");
-        loss++;
+        announcement.textContent = "You lost!";
+        computerScore.textContent = `Losses: ${++loss}`;
+    }
+
+    if ((wins === 5) || (loss === 5)) {
+        if (wins === 5) {
+            alert("YOU HAVE WON!")
+            wins = 0
+            tied = 0
+            loss = 0
+            computerScore.textContent = `Losses: ${loss}`;
+            playerScore.textContent = `Wins: ${wins}`;
+            totalTies.textContent = `Ties: ${tied}`;
+        }
+        else {
+            alert("The computer has beaten you...")
+            wins = 0
+            tied = 0
+            loss = 0
+            computerScore.textContent = `Losses: ${loss}`;
+            playerScore.textContent = `Wins: ${wins}`;
+            totalTies.textContent = `Ties: ${tied}`;
+        }
     }
 }
 
@@ -52,10 +91,30 @@ let tied = 0
 let playerChoice;
 let computerChoice;
 
-playGame(playerChoice, computerChoice)
-playGame(playerChoice, computerChoice)
-playGame(playerChoice, computerChoice)
-playGame(playerChoice, computerChoice)
-playGame(playerChoice, computerChoice)
+//Things to Manipulate
+const playerScore = document.querySelector('#player')
+const totalTies = document.querySelector('#ties')
+const computerScore = document.querySelector('#computer')
 
-console.log("You won " + wins + " times, lost " + loss + " times and tied " + tied + " times. Good job!")
+const announcement = document.querySelector('#announce');
+const playerChoiceDisplay = document.querySelector('#playerChoice');
+const computerChoiceDisplay = document.querySelector('#computerChoice');
+
+const choiceButtons = document.querySelector('#buttons');
+
+choiceButtons.addEventListener('click', (e) => {
+    choice = e.target.textContent;
+    playGame(choice);
+})
+
+
+if ((wins == 5) || loss == 5); 
+
+
+
+
+
+
+
+
+
